dc127df6e50a3be7067156780b063466dd81e079
UNNNYOU <unnnyou@gmail.com>
2024-06-14 16:00:18 +0900

    [improve] レイアウトの軽微な修正を行いました。

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -35,8 +35,8 @@ export default function Top() {
           fill
         />
       </div>
-      <div className="container mx-auto pt-10">
-        <div className="w-8/12 mx-auto">
+      <div className="container mx-auto pt-10 h-screen flex flex-col justify-center itemscenter">
+        <div className="w-full lg:w-8/12 mx-auto">
           <Image
             src="/logo.png"
             alt="トップページのロゴ"
@@ -44,22 +44,20 @@ export default function Top() {
             height="1080"
           />
         </div>
-        <div className="text-gray-700 text-center flex flex-col items-center justify-center">
-          <div className="mt-16">
-            <button
-              onClick={handleAuth}
-              className="relative text-center px-24 py-4 overflow-hidden font-bold text-gray-600 bg-green-300 border border-gray-100 rounded-sm shadow-inner group"
-            >
-              <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-              <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-              <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-              <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-              <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-              <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
-                戦いに行く
-              </span>
-            </button>
-          </div>
+        <div className="mt-16 text-gray-700 text-center">
+          <button
+            onClick={handleAuth}
+            className="relative text-center px-24 py-4 overflow-hidden font-bold text-gray-600 bg-green-300 border border-gray-100 rounded-sm shadow-inner group"
+          >
+            <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+            <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+            <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+            <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+            <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
+              戦いに行く
+            </span>
+          </button>
         </div>
       </div>
     </main>
