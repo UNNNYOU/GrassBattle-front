827cb221bb8c5206ed71b26fcaa19254fc3040e0
UNNNYOU <unnnyou@gmail.com>
2024-06-18 17:56:27 +0900

    [add] ヘッダー及びフッターの作成
    マイページ作成に付随してヘッダーとフッターも作成しました。

---
+++ src/components/sheard/footer.jsx
@@ -0,0 +1,11 @@
+export function Footer(){
+  return(
+    <footer className="bg-neutral-800 text-white">
+      <div className="container mx-auto text-center py-10">
+        <p className="text-sm">
+          © 2024 GRASS BATTLE All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
