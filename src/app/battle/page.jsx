67a0aa2a0d31cd15735202c3174424c4e206a1d0
UNNNYOU <unnnyou@gmail.com>
2024-06-21 10:16:23 +0900

    [add] ユーザー表示用のカード作成
    ユーザー一覧の表示に伴いローティング画面のMITライセンスの記載をソースコードに行いました。

--- src/app/battle/page.jsx
+++ src/app/battle/page.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Loader } from '@/components/sheard/loader';
 
-export default function Battle() {
+export default function Battle(){
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(true); // ローディング状態を追加
   const router = useRouter();
@@ -13,7 +13,7 @@ export default function Battle() {
   useEffect(() => {
     const access_token = localStorage.getItem('access_token');
     const refresh_token = localStorage.getItem('refresh_token');
-    const res = fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/battles`, {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/battles`, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${access_token}`
@@ -65,7 +65,35 @@ export default function Battle() {
   }
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="bg-white">
+      <div className="container m-auto min-h-screen">
+        <div className="text-center text-4xl pt-10">
+          <p>GRASS BATTLE MEMBERS</p>
+        </div>
+        <div className="flex flex-wrap justify-center my-10">
+          <div className="relative flex mx-4 mt-4 w-72 flex-col rounded-xl bg-white text-gray-700 shadow-2xl drop-shadow-xl">
+            <div className="relative mx-4 mt-4 h-44 overflow-hidden rounded-xl bg-white text-gray-700 shadow-xl">
+              <Image
+                src="/UserImage.png"
+                alt="ユーザー画像"
+                objectfit="cover"
+                fill
+              />
+            </div>
+            <p className="text-center mt-2 mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+              ユーザー名
+            </p>
+            <a href='#' className="relative text-center px-5 py-3 overflow-hidden font-bold text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group" >
+              <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+              <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+              <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+              <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+              <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
+              <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">BATTLE START</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
